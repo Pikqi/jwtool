f35a66cf789448d4deaf398b0fa48f98c4b20749
Petar Obradovic <perazmn@gmail.com>
2026-05-13 00:44:16 +0200

    Add readme; Add description to root cmd

---
+++ README.md
@@ -0,0 +1,9 @@
+### jwtool
+
+Alat pravljen za potrebe kursa bezbednost veb servisa
+
+### CLI interfejs
+
+jwtool koristi [cobra](https://cobra.dev/docs/) biblioteku za parsiranje cli komandi i argumenata.
+
+Dodavanje nove pod komande: `cobra-cli add serve`
--- cmd/root.go
+++ cmd/root.go
@@ -6,18 +6,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "jwtool",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
+	Short: "A tool for exploiting jwt vulnerabilities",
+	Long:  `A tool for exploiting jwt vulnerabilities`,
+	// TODO: Add parse jwt claims
 	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
@@ -39,5 +32,5 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("color", "t", true, "Toggle color mode")
 }
